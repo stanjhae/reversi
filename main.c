fa110bc09d6fe2d7509e50f432628bbd571a81d9
stanjhae <ceejayorji@yahoo.com>
2019-08-19 21:49:46 +0200

    Print x and o values. Added player 2. Added ability to flip.

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 
-int row = 0, col = 0, occupied;
+int playerRow = 0, playerCol = 0, occupied;
 void *levels[5][2] = { //levels for the game
         {"1", "Human"},
         {"2", "AI (Easy)"},
@@ -13,11 +13,19 @@ void *levels[5][2] = { //levels for the game
 char board[8][8];
 
 void printBoard() {
+    int x = 0, o = 0; //number of x and o
+
     for (int row = 0; row < 9; row++) {
         if (row != 0) { //if first row do not print number of row.
             printf("%d", row);
         }
         for (int col = 0; col < 9; col++) {
+            if (board[row + 1][col + 1] == 'x') { //if X, increase x.
+                x++;
+            }
+            if (board[row + 1][col + 1] == 'o') { //if O, increase o.
+                o++;
+            }
             if (row == 0) { //if first row
                 if (col != 0) { //and not first column
                     printf("%d\t", col); //print number of column
@@ -40,88 +48,147 @@ void printBoard() {
         }
         printf("\n\n");
     }
+    printf("X: %d,\t O: %d\n", x, o); //print result
 }
 
-int checkExtSpot(r, c, row, col) {
+int checkExtSpot(r, c, row, col, player) {
+    char x = 'x', o = 'o';
+    if (player == 2) {
+        x = 'o', o = 'x';
+    }
+
+//    if(row-2<1 || row+2>8 || col-2<1 || col+2>8){
+//        printf("row:%d, col:%d is outside the board\n", r, c);
+//        return 5;
+//    }
     printf("%d %d %d %d\n", r, c, row, col);
     int rowDiff = r - row; //finds row difference between possible flip and actual move
     int colDiff = c - col; //finds col difference between possible flip and actual move
 
     if (rowDiff == 0 && colDiff > 0) { //if same row but checking right col [0,2]
-        if (board[row][col + 2] == 'x') { //if spot contains opposite disc (x)
+        if (board[row][col + 2] == x) { //if spot contains opposite disc (x)
             printf("Found X at row:%d, col:%d\n", row, col + 2);
-        } else if (board[row][col + 2] == 'o') {
+            int reverseCol = col + 2;
+            while (reverseCol != playerCol) {
+                board[row][reverseCol - 1] = x;
+                reverseCol--;
+            }
+        } else if (board[row][col + 2] == o) {
             printf("Found o at row:%d, col:%d\n", row, col + 2);
-            checkExtSpot(r, c + 1, row, col + 1);
+            checkExtSpot(r, c + 1, row, col + 1, player);
         } else {
             printf("No X at row:%d, col:%d\n", row, col + 2);
         }
     } else if (rowDiff == 0 && colDiff < 0) { //if same row but checking left col [0,-2]
-        if (board[row][col - 2] == 'x') { //if spot contains opposite disc (x)
+        if (board[row][col - 2] == x) { //if spot contains opposite disc (x)
             printf("Found X at row:%d, col:%d\n", row, col - 2);
-        } else if (board[row][col - 2] == 'o') {
+            int reverseCol = col - 2;
+            while (reverseCol != playerCol) {
+                board[row][reverseCol + 1] = x;
+                reverseCol++;
+            }
+        } else if (board[row][col - 2] == o) {
             printf("Found o at row:%d, col:%d\n", row, col - 2);
-            checkExtSpot(r, c - 1, row, col - 1);
+            checkExtSpot(r, c - 1, row, col - 1, player);
         } else {
             printf("No X at row:%d, col:%d\n", row, col - 2);
 //                                    return 5; //throws a no flip possible error
         }
     } else if (rowDiff < 0 && colDiff == 0) { //if same col but checking top row [-2,0]
-        if (board[row - 2][col] == 'x') { //if spot contains opposite disc (x)
+        if (board[row - 2][col] == x) { //if spot contains opposite disc (x)
             printf("Found X at row:%d, col:%d\n", row - 2, col);
-        } else if (board[row - 2][col] == 'o') {
+            int reverseRow = row - 2;
+            while (reverseRow != playerRow) {
+                board[reverseRow + 1][col] = x;
+                reverseRow++;
+            }
+
+        } else if (board[row - 2][col] == o) {
             printf("Found o at row:%d, col:%d\n", row - 2, col);
-            checkExtSpot(r - 1, c, row - 1, col);
+            checkExtSpot(r - 1, c, row - 1, col, player);
         } else {
             printf("No X at row:%d, col:%d\n", row - 2, col);
 //                                    return 5; //throws a no flip possible error
         }
     } else if (rowDiff < 0 && colDiff < 0) { //if same col but checking top row [-2,-2]
-        if (board[row - 2][col - 2] == 'x') { //if spot contains opposite disc (x)
+        if (board[row - 2][col - 2] == x) { //if spot contains opposite disc (x)
             printf("Found X at row:%d, col:%d\n", row - 2, col - 2);
-        } else if (board[row - 2][col - 2] == 'o') {
+            int reverseRow = row - 2; //Get row where X or O is found
+            int reverseCol = col - 2; //Get col where X or O is found
+            while (reverseCol != playerCol) { //we loop until we get to initial play
+                board[reverseRow + 1][reverseCol + 1] = x; //change every character
+                reverseRow++; //increase or reduce row
+                reverseCol++; //increase or reduce col
+            }
+        } else if (board[row - 2][col - 2] == o) {
             printf("Found o at row:%d, col:%d\n", row - 2, col - 2);
-            checkExtSpot(r - 1, c - 1, row - 1, col - 1);
+            checkExtSpot(r - 1, c - 1, row - 1, col - 1, player);
         } else {
             printf("No X at row:%d, col:%d\n", row - 2, col - 2);
 //                                    return 5; //throws a no flip possible error
         }
     } else if (rowDiff < 0 && colDiff > 0) { //[-2,+2]
-        if (board[row - 2][col + 2] == 'x') { //if spot contains opposite disc (x)
+        if (board[row - 2][col + 2] == x) { //if spot contains opposite disc (x)
             printf("Found X at row:%d, col:%d\n", row - 2, col + 2);
-        } else if (board[row - 2][col + 2] == 'o') {
+            int reverseRow = row - 2;
+            int reverseCol = col + 2;
+            while (reverseCol != playerCol) {
+                board[reverseRow + 1][reverseCol - 1] = x;
+                reverseRow++;
+                reverseCol--;
+            }
+        } else if (board[row - 2][col + 2] == o) {
             printf("Found o at row:%d, col:%d\n", row - 2, col + 2);
-            checkExtSpot(r - 1, c + 1, row - 1, col + 1);
+            checkExtSpot(r - 1, c + 1, row - 1, col + 1, player);
         } else {
             printf("No X at row:%d, col:%d\n", row - 2, col + 2);
 //                                    return 5; //throws a no flip possible error
         }
     } else if (rowDiff > 0 && colDiff < 0) { //[2,-2]
-        if (board[row + 2][col - 2] == 'x') { //if spot contains opposite disc (x)
+        if (board[row + 2][col - 2] == x) { //if spot contains opposite disc (x)
             printf("Found X at row:%d, col:%d\n", row + 2, col - 2);
-        } else if (board[row + 2][col - 2] == 'o') {
+            int reverseRow = row + 2;
+            int reverseCol = col - 2;
+            while (reverseCol != playerCol) {
+                board[reverseRow - 1][reverseCol + 1] = x;
+                reverseRow--;
+                reverseCol++;
+            }
+        } else if (board[row + 2][col - 2] == o) {
             printf("Found o at row:%d, col:%d\n", row + 2, col - 2);
-            checkExtSpot(r + 1, c - 1, row + 1, col + 1);
+            checkExtSpot(r + 1, c - 1, row + 1, col + 1, player);
         } else {
             printf("No X at row:%d, col:%d\n", row + 2, col - 2);
 //                                    return 5; //throws a no flip possible error
         }
     } else if (rowDiff > 0 && colDiff > 0) { //[2,2]
-        if (board[row + 2][col + 2] == 'x') { //if spot contains opposite disc (x)
+        if (board[row + 2][col + 2] == x) { //if spot contains opposite disc (x)
             printf("Found X at row:%d, col:%d\n", row + 2, col + 2);
-        } else if (board[row + 2][col + 2] == 'o') {
+            int reverseRow = row + 2;
+            int reverseCol = col + 2;
+            while (reverseCol != playerCol) {
+                board[reverseRow - 1][reverseCol - 1] = x;
+                reverseRow--;
+                reverseCol--;
+            }
+        } else if (board[row + 2][col + 2] == o) {
             printf("Found o at row:%d, col:%d\n", row - 2, col + 2);
-            checkExtSpot(r + 1, c + 1, row + 1, col + 1);
+            checkExtSpot(r + 1, c + 1, row + 1, col + 1, player);
         } else {
             printf("No X at row:%d, col:%d\n", row + 2, col + 2);
 //                                    return 5; //throws a no flip possible error
         }
     } else if (rowDiff > 0 && colDiff == 0) { //if same col but checking bottom row [2,0]
-        if (board[row + 2][col] == 'x') { //if spot contains opposite disc (x)
+        if (board[row + 2][col] == x) { //if spot contains opposite disc (x)
             printf("Found X at row:%d, col:%d\n", row + 2, col);
-        } else if (board[row + 2][col] == 'o') {
+            int reverseRow = row + 2;
+            while (reverseRow != playerRow) {
+                board[reverseRow - 1][col] = x;
+                reverseRow--;
+            }
+        } else if (board[row + 2][col] == o) {
             printf("Found o at row:%d, col:%d\n", row + 2, col);
-            checkExtSpot(r + 1, c, row + 1, col);
+            checkExtSpot(r + 1, c, row + 1, col, player);
         } else {
             printf("No X at row:%d, col:%d\n", row + 2, col);
 //                                    return 5; //throws a no flip possible error
@@ -130,8 +197,13 @@ int checkExtSpot(r, c, row, col) {
 
 }
 
-int checkOccupied(row, col) {
-    if (board[row][col] == 'x' || board[row][col] == 'o') { //if spot is occupied
+int checkOccupied(row, col, player) {
+    char x = 'x', o = 'o';
+    if (player == 2) {
+        x = 'o', o = 'x';
+    }
+
+    if (board[row][col] == x || board[row][col] == o) { //if spot is occupied
         printf("This spot is occupied");
         return 2; //throws an occupied error
     } else {
@@ -142,17 +214,17 @@ int checkOccupied(row, col) {
 
             for (int c = col - 1; c <= col + 1; c++) { //check the spots around a given point
 
-                if ((board[r][c] == 'x') || (board[r][c] == 'o')) { //if any spot around the given point is occupied
+                if ((board[r][c] == x) || (board[r][c] == o)) { //if any spot around the given point is occupied
                     found++; //increment found
                     printf("%c\n", board[r][c]);
-                    if (board[r][c] == 'o') { //for player 'x', check if neighbour is opposite 'o'
+                    if (board[r][c] == o) { //for player x, check if neighbour is opposite o
                         opposite++; //increment opposite
 
-                        checkExtSpot(r, c, row, col);
+                        checkExtSpot(r, c, row, col, player);
                         rounds++;
-//                        if(board[r][c+1] == 'x'){
+//                        if(board[r][c+1] == x){
 //                            printf("Found X at row:%d, col:%d\n", r, c+1);
-//                        }else if(board[r+1][c+1] == 'x'){
+//                        }else if(board[r+1][c+1] == x){
 //                            printf("Found X at row:%d, col:%d\n", r+1, c+1);
 //                        }else
 //                            printf("No X found");
@@ -179,17 +251,12 @@ int checkOccupied(row, col) {
 }
 
 int main() {
+    int play = 1;
 
-    board[4][4] = 'o';
+    board[4][4] = 'x';
     board[4][5] = 'o';
     board[5][4] = 'o';
-    board[5][5] = 'o';
-    board[4][3] = 'o';
-    board[3][3] = 'x';
-    board[5][2] = 'o';
-    board[5][1] = 'o';
-    board[5][6] = 'o';
-    board[3][5] = 'o';
+    board[5][5] = 'x';
 
     int player1 = 1, player2 = 1; //define players
 
@@ -246,19 +313,37 @@ int main() {
 //
 //    }
 
-    printf("Player 1 move: ");
-    scanf("%d, %d", &row, &col); //getting the player's move (row,col)
+    while (play) { //Continue playing until play becomes false.
+        printf("Player 1 move: ");
+        scanf("%d, %d", &playerRow, &playerCol); //getting the player's move (row,col)
 
-    occupied = checkOccupied(row, col);
+        if (playerRow == '9') {
+            break;
+        }
+        occupied = checkOccupied(playerRow, playerCol, 1);
 
-    if (occupied != 0) {
-        return occupied;
+        if (occupied != 0) {
+            return occupied;
+        }
+
+        board[playerRow][playerCol] = 'x'; //assign x to player's move
+
+        printBoard(); //print updated board
+
+        printf("Player 2 move: ");
+        scanf("%d, %d", &playerRow, &playerCol); //getting the player's move (row,col)
+
+        occupied = checkOccupied(playerRow, playerCol, 2);
+
+        if (occupied != 0) {
+            return occupied;
+        }
+
+        board[playerRow][playerCol] = 'o'; //assign x to player's move
+
+        printBoard(); //print updated board
+
     }
-
-    board[row][col] = 'x'; //assign x to player's move
-
-    printBoard(); //print updated board
-
     printf("GAME OVER!!");
 
     return 0;
